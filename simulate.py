0d0df84c39de032dd07efd4c3d0e819182f79038
saywar <saidyakabjk@gmail.com>
2023-01-05 23:24:00 -0600

    Starting work on simulation.

--- simulate.py
+++ simulate.py
@@ -1,2 +1,10 @@
-pass
-#Lets Star
+import pybullet as p
+import time
+#Lets Start
+physicsClient = p.connect(p.GUI)
+for i in range(1000):
+    p.stepSimulation()
+    time.sleep(1/60)
+    print(i)
+p.disconnect()
+
