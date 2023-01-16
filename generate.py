748778b7b530f43ad9df9ad2ef57205e59b9dd4c
saywar <saidyakabjk@gmail.com>
2023-01-15 18:42:25 -0600

    last changes

--- generate.py
+++ generate.py
@@ -1,17 +1,24 @@
 #Start
 import pyrosim.pyrosim as pyrosim
 
-pyrosim.Start_SDF("boxes.sdf")
+
+def Create_Robot(): 
+    pyrosim.Start_URDF("body.urdf")
+    pyrosim.Send_Cube(name="BackLeg", pos=[0,0,0.5] , size=[1,1,1])
+    pyrosim.Send_Joint( name = "BackLeg_Torso" , parent= "BackLeg" , child = "Torso" , type = "revolute", position = [0,0,1])
+    pyrosim.Send_Cube(name="Torso", pos=[1,0,0.5] , size=[1,1,1])
+    pyrosim.Send_Joint( name = "Torso_FrontLeg" , parent= "Torso" , child = "FrontLeg" , type = "revolute", position = [1,0,-1])
+    pyrosim.Send_Cube(name="FrontLeg", pos=[1,0,0.5] , size=[1,1,1])
+    pyrosim.End()
+
+def Create_World():
+    pyrosim.Start_SDF("world.sdf")
 
 
 
-for j in range(5):
-    for k in range(5):
-        size = 1 
-        for i in range(10):
+    pyrosim.Send_Cube(name="box", pos=[0,5,0] , size=[1,1,1])
 
-            
-            size = 0.90**i
-            pyrosim.Send_Cube(name="boxes.sdf", pos=[k,j,i] , size=[size,size,size])
+    pyrosim.End()
 
-pyrosim.End()
+Create_World()
+Create_Robot()
