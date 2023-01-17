1495659b31653b7ab4967cb110d502ea09b91dac
saywar <saidyakabjk@gmail.com>
2023-01-16 21:56:41 -0600

    moving robot

--- generate.py
+++ generate.py
@@ -3,13 +3,17 @@ import pyrosim.pyrosim as pyrosim
 
 
 def Create_Robot(): 
+
     pyrosim.Start_URDF("body.urdf")
-    pyrosim.Send_Cube(name="BackLeg", pos=[0,0,0.5] , size=[1,1,1])
-    pyrosim.Send_Joint( name = "BackLeg_Torso" , parent= "BackLeg" , child = "Torso" , type = "revolute", position = [0,0,1])
-    pyrosim.Send_Cube(name="Torso", pos=[1,0,0.5] , size=[1,1,1])
-    pyrosim.Send_Joint( name = "Torso_FrontLeg" , parent= "Torso" , child = "FrontLeg" , type = "revolute", position = [1,0,-1])
-    pyrosim.Send_Cube(name="FrontLeg", pos=[1,0,0.5] , size=[1,1,1])
+    pyrosim.Send_Cube(name='Torso', pos=[1.5,0,1.5] , size=[1,1,1])
+    pyrosim.Send_Joint( name = "Torso_BackLeg" , parent= "Torso" , child = "Backleg" , type = "revolute", position = [1,0,1])
+    pyrosim.Send_Cube(name='Backleg', pos=[-.5,0,-0.5] , size=[1,1,1])
+    pyrosim.Send_Joint( name = "Torso_FrontLeg" , parent= "Torso" , child = "Frontleg" , type = "revolute", position = [2, 0, 1])
+    pyrosim.Send_Cube(name='Frontleg', pos=[0.5,0,-0.5] , size=[1,1,1])
+
     pyrosim.End()
+
+
 
 def Create_World():
     pyrosim.Start_SDF("world.sdf")
--- simulate.py
+++ simulate.py
@@ -1,16 +1,62 @@
 import pybullet as p
 import time
 import pybullet_data
+import pyrosim.pyrosim as pyrosim
+import numpy
+import random
 #Lets Start
+back_amplitude = numpy.pi/2
+back_frequency = 25
+back_phaseOffset = -numpy.pi/2
+#front
+front_amplitude = numpy.pi/2
+front_frequency = 25
+front_phaseOffset = -numpy.pi/2
+
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 p.setGravity(0,0,-9.8)
 planeId = p.loadURDF("plane.urdf")
 robotId = p.loadURDF("body.urdf")
 p.loadSDF("world.sdf")
+pyrosim.Prepare_To_Simulate(robotId)
+backLegSensorValues = numpy.zeros(1000)
+FrontLegSensorValues = numpy.zeros(1000)
+
+back_targetAngles = numpy.linspace(0, 2*numpy.pi, 1000)
+front_targetAngles = numpy.linspace(0, 2*numpy.pi, 1000)
+for i in range(1000):
+    back_targetAngles[i] = back_amplitude * numpy.sin(back_frequency * back_targetAngles[i] + back_phaseOffset) 
+    front_targetAngles[i]  = front_amplitude * numpy.sin(front_frequency * front_targetAngles[i] + front_phaseOffset) 
+
+numpy.save('data/back_sins.npy', back_targetAngles, allow_pickle=True, fix_imports=True)
+numpy.save('data/front_sins.npy', front_targetAngles, allow_pickle=True, fix_imports=True)
+
+
+
 for i in range(1000):
     p.stepSimulation()
+   # backLegSensorValues[i]= pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
+    #FrontLegSensorValues[i]= pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
+    pyrosim.Set_Motor_For_Joint(
+        bodyIndex = robotId,
+        jointName = b'Torso_BackLeg',
+        controlMode = p.POSITION_CONTROL,  
+        targetPosition = back_targetAngles[i],
+        maxForce = 100)
+    pyrosim.Set_Motor_For_Joint(
+        bodyIndex = robotId,
+        jointName = b'Torso_FrontLeg',
+        controlMode = p.POSITION_CONTROL,  
+        targetPosition = front_targetAngles[i],
+        maxForce = 100)
+
+
     time.sleep(1/60)
     print(i)
-p.disconnect()
+    #print(i)
+numpy.save('data/info.npy', backLegSensorValues, allow_pickle=True, fix_imports=True)
+numpy.save('data/frontInfo.npy', backLegSensorValues, allow_pickle=True, fix_imports=True)
+
+p.disconnect()  
 
