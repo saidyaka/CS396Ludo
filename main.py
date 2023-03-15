983e0d9bcd210389041a5e43af6dbf66c71ebdbb
saywar <saidyakabjk@gmail.com>
2023-03-15 00:17:44 -0500

    FINISHED

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ import sys
 
 show = sys.argv[1]
 
+os.system('pip3 install -r requirements.txt')
 if show == "show":
     os.system('python3 simulate.py GUI 0 E')
     os.system('python3 simulate.py GUI 0 show')
-else:
 
-    os.system('pip3 install -r requirements.txt')
+else:
     os.system('python3 search.py')
